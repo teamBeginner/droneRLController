73a0adfd66ac9187bfca93b6c915441ce0196158
WaaagH <zmkyz@sina.com>
2018-08-28 16:54:07 +0800

    update
    update step function using Runge-Kutta methods

--- quard-copter_Dynamics_ZYZ/quard_copter.py
+++ quard-copter_Dynamics_ZYZ/quard_copter.py
@@ -53,7 +53,7 @@ class quard_copter(object):
         P,Speed,Angle,pqr = state
         phi_threshold = 0.1
         theta_threshold = 0.1
-        safe_zone = np.array([5,5,5])
+        safe_zone = np.array([2,2,2])
         
         done = False
         done = np.abs(Angle[0]) > phi_threshold or\
@@ -69,36 +69,79 @@ class quard_copter(object):
             reward = 0
         
         return reward,done
-    
-    def step(self,Force,reward_fn=check_stable_stat,dt=0.01):
-        self.Force = Force
+
+    def grad(self,Force,state):
+
+        [P,Speed,Angle,pqr] = state
         
-        
-        sins = np.sin(self.Angle)
-        coss = np.cos(self.Angle)
+        sins = np.sin(Angle)
+        coss = np.cos(Angle)
         
         mass = self.Mass
-        self.Acc[0] = np.sum(Force*(sins[0]*sins[2]+coss[0]*sins[1]*coss[2]))/mass
-        self.Acc[1] = np.sum(Force*(-sins[0]*coss[2]+coss[0]*sins[1]*sins[2]))/mass
-        self.Acc[2] = np.sum(Force*(coss[0]*coss[1]))/mass-9.8
         
-        p,q,r = self.pqr
-        self.dAngle[0] = (p*coss[1]+q*sins[0]*sins[1]+r*coss[0]*sins[1])/coss[1]
-        self.dAngle[1] = q*coss[0]+r*sins[0]
-        self.dAngle[2] = (q*sins[0]+r*coss[0])/coss[1]
+        Acc = np.zeros(3)
+        Acc[0] = np.sum(Force*(sins[0]*sins[2]+coss[0]*sins[1]*coss[2]))/mass
+        Acc[1] = np.sum(Force*(-sins[0]*coss[2]+coss[0]*sins[1]*sins[2]))/mass
+        Acc[2] = np.sum(Force*(coss[0]*coss[1]))/mass-9.8
+        
+        p,q,r = pqr
+        
+        dAngle = np.zeros(3)
+        dAngle[0] = (p*coss[1]+q*sins[0]*sins[1]+r*coss[0]*sins[1])/coss[1]
+        dAngle[1] = q*coss[0]+r*sins[0]
+        dAngle[2] = (q*sins[0]+r*coss[0])/coss[1]
         
         Ix,Iy,Iz = self.I
         L = self.L
-        self.dpqr[0] = (L*(Force[3]-Force[1])+q*r*(Iy-Iz))/Ix
-        self.dpqr[1] = (L*(Force[2]-Force[0])+p*r*(Iz-Ix))/Iy
-        self.dpqr[2] = (L*(Force[1]+Force[3]-Force[0]-Force[2])+q*r*(Ix-Iy))/Iz
         
-        self.P += self.Speed*dt
-        self.Speed += self.Acc*dt
-        self.Angle += self.dAngle*dt
-        self.pqr += self.dpqr*dt
+        dpqr = np.zeros(3)
+        dpqr[0] = (L*(Force[3]-Force[1])+q*r*(Iy-Iz))/Ix
+        dpqr[1] = (L*(Force[2]-Force[0])+p*r*(Iz-Ix))/Iy
+        dpqr[2] = (L*(Force[1]+Force[3]-Force[0]-Force[2])+q*r*(Ix-Iy))/Iz
         
-        state = [self.P,self.Speed,self.Angle,self.pqr]
+        grad = [Speed,Acc,dAngle,dpqr]
+        return grad
+
+    def step(self,Force,reward_fn=check_stable_stat,dt=0.02):
+        self.Force = Force
+        
+        state_1 = [self.P,self.Speed,self.Angle,self.pqr]
+        h = dt/4.
+        k1 = self.grad(Force,state_1)
+        state_2 = [state_1[i]+h*k1[i] for i in range(len(k1))]
+        k2 = self.grad(Force,state_2)
+        state_3 = [state_2[i]+h*k2[i] for i in range(len(k1))]
+        k3 = self.grad(Force,state_3)
+        state_4 = [state_3[i]+h*k3[i] for i in range(len(k1))]
+        k4 = self.grad(Force,state_4)
+        state = [state_1[i]+h/6.*(k1[i]+2*k2[i]+2*k3[i]+k4[i]) for i in range(len(k1))]
+        
+#        sins = np.sin(self.Angle)
+#        coss = np.cos(self.Angle)
+#        
+#        mass = self.Mass
+#        self.Acc[0] = np.sum(Force*(sins[0]*sins[2]+coss[0]*sins[1]*coss[2]))/mass
+#        self.Acc[1] = np.sum(Force*(-sins[0]*coss[2]+coss[0]*sins[1]*sins[2]))/mass
+#        self.Acc[2] = np.sum(Force*(coss[0]*coss[1]))/mass-9.8
+#        
+#        p,q,r = self.pqr
+#        self.dAngle[0] = (p*coss[1]+q*sins[0]*sins[1]+r*coss[0]*sins[1])/coss[1]
+#        self.dAngle[1] = q*coss[0]+r*sins[0]
+#        self.dAngle[2] = (q*sins[0]+r*coss[0])/coss[1]
+#        
+#        Ix,Iy,Iz = self.I
+#        L = self.L
+#        self.dpqr[0] = (L*(Force[3]-Force[1])+q*r*(Iy-Iz))/Ix
+#        self.dpqr[1] = (L*(Force[2]-Force[0])+p*r*(Iz-Ix))/Iy
+#        self.dpqr[2] = (L*(Force[1]+Force[3]-Force[0]-Force[2])+q*r*(Ix-Iy))/Iz
+#        
+#        self.P += self.Speed*dt
+#        self.Speed += self.Acc*dt
+#        self.Angle += self.dAngle*dt
+#        self.pqr += self.dpqr*dt
+#        state = [self.P,self.Speed,self.Angle,self.pqr]
+        
+        self.P,self.Speed,self.Angle,self.pqr = state
         reward,done = reward_fn(self,state)
         
         return state,reward,done
@@ -117,6 +160,8 @@ class quard_copter(object):
         return state
 
 
+q = quard_copter()
+print(q.step(np.zeros(4)))
 
 
 
